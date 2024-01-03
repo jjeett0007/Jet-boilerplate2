cd565d422d82ecffb3b4633e4c4e6ac0da1c01d5
jjeett0007 <dosumuolayinka150@gmail.com>
2024-01-03 17:55:37 +0000

    Code Synced by DhiWise

---
+++ routes/device/v1/index.js
@@ -0,0 +1,28 @@
+/**
+ * index.js
+ * @description :: index route file of device platform.
+ */
+
+const express =  require('express');
+const router =  express.Router();
+router.use('/device/auth',require('./auth'));
+router.use(require('./departmentsRoutes'));
+router.use(require('./noteRoutes'));
+router.use(require('./encounterRoutes'));
+router.use(require('./orderRoutes'));
+router.use(require('./CustomerRoutes'));
+router.use(require('./PlanRoutes'));
+router.use(require('./TaskRoutes'));
+router.use(require('./Chat_messageRoutes'));
+router.use(require('./CommentRoutes'));
+router.use(require('./Chat_groupRoutes'));
+router.use(require('./ToDoRoutes'));
+router.use(require('./Appointment_scheduleRoutes'));
+router.use(require('./EventRoutes'));
+router.use(require('./Appointment_slotRoutes'));
+router.use(require('./MasterRoutes'));
+router.use(require('./BlogRoutes'));
+router.use(require('./userRoutes'));
+router.use(require('./uploadRoutes'));
+
+module.exports = router;
